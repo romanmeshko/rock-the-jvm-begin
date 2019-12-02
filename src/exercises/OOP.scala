bb58827f376253b043be877a72e3d14b3aa40e49
Roman Meshko <roman.meshko@gmail.com>
2019-12-02 22:54:24 +0200

    ren

--- src/exercises/OOP.scala
+++ src/exercises/OOP.scala
@@ -2,10 +2,29 @@ package exercises
 
 object OOP extends App {
   val writer = new Writer("John", "Sedgewick", 55)
-  writer.fullname
+  writer.fullname()
 }
 
 class Writer(val firstName: String, val surname: String, val year: Int) {
   def fullname = s"${this.firstName} ${this.surname}"
 }
 
+class Novel(val name: String, val releaseYear: Int, val writer: Writer) {
+  def authorAge: Int = this.releaseYear - writer.year
+
+  def isWrittenBy: Writer = writer
+
+  def copy(newYearOfRelease: Int) = new Novel(this.name, newYearOfRelease, writer)
+}
+
+class Counter(val value: Int) {
+  def currentCount: Int = this.value
+
+  def inc(delta: Int): Counter = new Counter(this.value + delta)
+
+  def dec(delta: Int): Counter = new Counter(this.value - delta)
+
+  def inc: Counter = inc(1)
+
+  def dec: Counter = dec(1)
+}
