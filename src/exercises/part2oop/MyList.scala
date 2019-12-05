89f427b5122552c661182ddc33d483f2d70ca34b
Roman Meshko <roman.meshko@loopme.com>
2019-12-05 17:31:58 +0200

    generic list

--- src/exercises/part2oop/MyList.scala
+++ src/exercises/part2oop/MyList.scala
@@ -2,26 +2,26 @@ package exercises.part2oop
 
 abstract class MyList {
   def head: Int
-  def tail: GList
+  def tail: MyList
   def isEmpty: Boolean
-  def add(element: Int): GList
+  def add(element: Int): MyList
   override def toString: String = s"[ $printElements ]"
   def printElements: String
 }
 
-object EmptyList extends GList {
+object EmptyList extends MyList {
   override def head: Int = throw new IllegalStateException
-  override def tail: GList = throw new IllegalStateException
+  override def tail: MyList = throw new IllegalStateException
   override def isEmpty: Boolean = true
-  override def add(element: Int): GList = new Cons(element, this)
+  override def add(element: Int): MyList = new Cons(element, this)
   override def printElements: String = ""
 }
 
-class Cons(element: Int, t: GList) extends GList {
+class Cons(element: Int, t: MyList) extends MyList {
   override def head: Int = element
-  override def tail: GList = t
+  override def tail: MyList = t
   override def isEmpty: Boolean = false
-  override def add(element: Int): GList = new Cons(element, this)
+  override def add(element: Int): MyList = new Cons(element, this)
   override def printElements: String =
     if (t.isEmpty) s"$element"
     else s"$element ${t.printElements}"
