245418e386be92383f3decc2ab3208b03d5da072
Roman Meshko <roman.meshko@gmail.com>
2019-12-06 23:28:18 +0200

    GList implementation

--- src/exercises/part2oop/GenericList.scala
+++ src/exercises/part2oop/GenericList.scala
@@ -7,6 +7,10 @@ abstract class GList[+A] {
   def add[B >: A](element: B): GList[B]
   override def toString: String = s"[ $printElements ]"
   def printElements: String
+  def map[B](transformer: MyTransformer[A, B]): GList[B]
+  def filter(predicate: MyPredicate[A]): GList[A]
+  def flatMap[B](transformer: MyTransformer[A, GList[B]]): GList[B]
+  def ++[B >: A](list: GList[B]): GList[B]
 }
 
 object EmptyList extends GList[Nothing] {
@@ -15,6 +19,10 @@ object EmptyList extends GList[Nothing] {
   override def isEmpty: Boolean = true
   override def add[B >: Nothing](element: B): GList[B] = new Cons(element, this)
   override def printElements: String = ""
+  override def map[B](transformer: MyTransformer[Nothing, B]): GList[B] = this
+  override def filter(predicate: MyPredicate[Nothing]): GList[Nothing] = this
+  override def flatMap[B](transformer: MyTransformer[Nothing, GList[B]]): GList[B] = this
+  override def ++[B >: Nothing](list: GList[B]): GList[B] = list
 }
 
 class Cons[+A](element: A, t: GList[A]) extends GList[A] {
@@ -25,4 +33,20 @@ class Cons[+A](element: A, t: GList[A]) extends GList[A] {
   override def printElements: String =
     if (t.isEmpty) s"$element"
     else s"$element ${t.printElements}"
+  override def map[B](transformer: MyTransformer[A, B]): GList[B] =
+    new Cons(transformer.transform(element), t.map(transformer))
+  override def filter(predicate: MyPredicate[A]): GList[A] =
+    if (predicate.test(element)) new Cons(element, t.filter(predicate))
+    else t.filter(predicate)
+  override def ++[B >: A](list: GList[B]): GList[B] = new Cons[B](element, t ++ list)
+  override def flatMap[B](transformer: MyTransformer[A, GList[B]]): GList[B] =
+    transformer.transform(element) ++ t.flatMap(transformer)
+}
+
+trait MyPredicate[-T] {
+  def test(t: T): Boolean
+}
+
+trait MyTransformer[-A, B] {
+  def transform(a: A): B
 }
