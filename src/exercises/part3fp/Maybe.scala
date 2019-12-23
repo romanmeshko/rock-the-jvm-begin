44d5d9cd536938ab9cce26a9cada4984b4dceed2
roman-meshko <roman.meshko@loopme.com>
2019-12-23 15:02:18 +0200

    Maybe

--- src/exercises/part3fp/Maybe.scala
+++ src/exercises/part3fp/Maybe.scala
@@ -1,11 +1,27 @@
 package exercises.part3fp
 
-class Maybe[+T](element: T) {
-  def filter(pred: T => Boolean): T =
-    if (element == null) null
-    else if (pred(element)) element
-    else null
-  def map[U](f: T => U): U
+abstract class Maybe[+T] {
+  def filter(p: T => Boolean): Maybe[T]
+
+  def map[U](f: T => U): Maybe[U]
+
+  def flatMap[U](f: T => Maybe[U]): Maybe[U]
 }
 
-object None extends Maybe[Nothing]
+case object None extends Maybe[Nothing] {
+  override def filter(p: Nothing => Boolean): Maybe[Nothing] = this
+
+  override def map[U](f: Nothing => U): Maybe[U] = this
+
+  override def flatMap[U](f: Nothing => Maybe[U]): Maybe[U] = this
+}
+
+case class Just[T](value: T) extends Maybe[T] {
+  override def filter(p: T => Boolean): Maybe[T] =
+    if (p(value)) this
+    else None
+
+  override def map[U](f: T => U): Maybe[U] = Just(f(value))
+
+  override def flatMap[U](f: T => Maybe[U]): Maybe[U] = f(value)
+}
